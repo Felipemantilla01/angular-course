cb03cd097c61969413eb52e61c494b1530d555df
Andres Felipe Mantilla Gomez <amantilla@SYSMAN.COM.CO>
2020-01-21 10:33:47 -0500

    interpolations reach

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  loginBtnText = 'Login button'
+
+  public name = 'Felipe'
+  public siteUrl = window.location.href
   constructor() {}
 
+  helloWorld(){
+    return 'hello world by ' + this.name 
+  }
   ngOnInit() {
   }
 
