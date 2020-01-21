a34a7950a828852e1e9610a9ce34f5d6a4ce162b
Andres Felipe Mantilla Gomez <amantilla@SYSMAN.COM.CO>
2020-01-21 12:10:10 -0500

    binding classes and conditionals

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  public textClass = "text-success"
+  public hasError = true
+  public hasError2 = false
 
+  public isSpecial = true
+  messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+    }
   constructor() {}
 
   ngOnInit() {
