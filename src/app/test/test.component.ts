e6f8c59c6a784c56913a20ebb572edec284a0494
Andres Felipe Mantilla Gomez <amantilla@SYSMAN.COM.CO>
2020-01-21 15:53:39 -0500

    init

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,11 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  public display = false
-  public toggle(){
-    this.display= !this.display
-  }
-  
   constructor() {}
 
   ngOnInit() {
