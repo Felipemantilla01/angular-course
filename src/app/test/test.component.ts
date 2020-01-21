2a2f9e85b19599a899bd155ecea0cc9d42993774
Andres Felipe Mantilla Gomez <amantilla@SYSMAN.COM.CO>
2020-01-21 15:23:57 -0500

    two way binding test with ngModel --> @angular/forms

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  
+  public name = ""
+  public logger(e){
+    console.log(e)
+  }
   constructor() {}
 
   ngOnInit() {
